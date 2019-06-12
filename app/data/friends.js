261a455a057db9bcba893284ec2f6f7f35ec1458
unknown <cortikalch@hotmail.com>
2019-06-11 21:19:04 -0500

    Advance 2, working in logic for friends

---
+++ app/data/friends.js
@@ -0,0 +1,14 @@
+var friends = [
+    {
+      name: "Ahmed",
+      photo: "https://picsum.photos/200/300",
+      scores:["5", "1", "4", "4", "5", "1", "2", "5", "4", "1"]
+    }
+  ];
+  
+  // Note how we export the array. This makes it accessible to other files using require.
+  module.exports = friends;
+  
+
+
+
